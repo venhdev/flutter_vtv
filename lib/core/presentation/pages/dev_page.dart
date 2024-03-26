d3f5f059a59b3c3d09d9b6fe46918c3811961613
Venh <venh.ha@gmail.com>
2024-03-26 19:55:19 +0700

    fix checkToken save to local

--- lib/core/presentation/pages/dev_page.dart
+++ lib/core/presentation/pages/dev_page.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_vtv/core/helpers/secure_storage_helper.dart';
 import 'package:flutter_vtv/core/helpers/shared_preferences_helper.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../../features/auth/presentation/bloc/auth_cubit.dart';
 import '../../../service_locator.dart';
 import '../../constants/api.dart';
 
@@ -16,20 +20,41 @@ class DevPage extends StatefulWidget {
 }
 
 class _DevPageState extends State<DevPage> {
+  String accessToken = '';
+  String? newAccessToken;
+
   Future<void> setDomain(String newDomain) async {
     if (newDomain == devDOMAIN) {
       Fluttertoast.showToast(msg: 'Domain is the same');
     } else {
       devDOMAIN = newDomain;
       await sl<SharedPreferencesHelper>().I.setString('devDomain', devDOMAIN);
-      Fluttertoast.showToast(
-          msg: 'Server domain has been changed to $devDOMAIN');
+      Fluttertoast.showToast(msg: 'Server domain has been changed to $devDOMAIN');
       setState(() {});
     }
   }
 
-  TextEditingController domainTextController =
-      TextEditingController(text: devDOMAIN);
+  TextEditingController domainTextController = TextEditingController(text: devDOMAIN);
+
+  @override
+  void initState() {
+    super.initState();
+    domainTextController.text = devDOMAIN;
+    sl<SecureStorageHelper>().accessToken.then((token) {
+      if (mounted && token != null) {
+        Fluttertoast.showToast(msg: 'loaded access token');
+        setState(() {
+          accessToken = token;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    domainTextController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,31 +68,85 @@ class _DevPageState extends State<DevPage> {
             icon: const Icon(Icons.arrow_back),
           )),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('current Domain: $devDOMAIN'),
-            TextField(
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(),
-                labelText: 'Domain',
-                suffixIcon: IconButton(
-                  onPressed: () async {
-                    await setDomain(domainTextController.text);
-                  },
-                  icon: const Icon(Icons.save),
-                ),
-              ),
-              // init value
-              controller: domainTextController,
-              // save button
-              onSubmitted: (value) async {
-                await setDomain(value);
-              },
-            ),
-          ],
-        ),
+        child: _buildContent(),
       ),
     );
+  }
+
+  Column _buildContent() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text('current Domain: $devDOMAIN'),
+        TextField(
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            labelText: 'Domain',
+            suffixIcon: IconButton(
+              onPressed: () async {
+                await setDomain(domainTextController.text);
+              },
+              icon: const Icon(Icons.save),
+            ),
+          ),
+          // init value
+          controller: domainTextController,
+          // save button
+          onSubmitted: (value) async {
+            await setDomain(value);
+          },
+        ),
+
+        // other test
+        const SizedBox(height: 20),
+        GestureDetector(
+          onTap: () {
+            Clipboard.setData(ClipboardData(text: accessToken));
+            Fluttertoast.showToast(msg: 'Copied to clipboard');
+          },
+          child: Text('accessToken: $accessToken'),
+        ),
+        ElevatedButton(
+          onPressed: () async {
+            final accessToken = await sl<SecureStorageHelper>().accessToken;
+            if (mounted && accessToken != null) {
+              setState(() {
+                Fluttertoast.showToast(msg: 'get current access token success');
+                this.accessToken = accessToken;
+              });
+            }
+          },
+          child: const Text('Get current access token'),
+        ),
+
+        // call usecase to get new access token
+        GestureDetector(
+          child: Text('newAccessToken: $newAccessToken'),
+          onTap: () {
+            Clipboard.setData(ClipboardData(text: newAccessToken ?? ''));
+            Fluttertoast.showToast(msg: 'Copied to clipboard');
+          },
+        ),
+        const SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: () async {
+            // final result = await sl<CheckTokenUC>().call(accessToken);
+            await sl<AuthCubit>().onStarted();
+            // Fluttertoast.showToast(msg: 'result: $result');
+            final currentToken = getCurrentToken();
+            if (mounted && currentToken != null) {
+              setState(() {
+                newAccessToken = currentToken;
+              });
+            }
+          },
+          child: const Text('Check and get new access token'),
+        ),
+      ],
+    );
+  }
+
+  String? getCurrentToken() {
+    return context.read<AuthCubit>().state.auth?.accessToken;
   }
 }
--- lib/features/auth/presentation/bloc/auth_cubit.dart
+++ lib/features/auth/presentation/bloc/auth_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -37,16 +39,19 @@ class AuthCubit extends Cubit<AuthState> {
         (failure) => emit(AuthState.error(message: failure.message)),
         (authEntity) async {
           // get new access token if needed
-          final resultCheck =
-              await _checkAndGetTokenIfNeededUC(authEntity.accessToken);
+          final resultCheck = await _checkAndGetTokenIfNeededUC(authEntity.accessToken);
+
           resultCheck.fold(
             // get new access token failed
-            (failure) => emit(
-                AuthState.authenticated(authEntity, message: failure.message)),
-            (newAccessToken) => emit(
-              AuthState.authenticated(
-                  authEntity.copyWith(accessToken: newAccessToken)),
-            ),
+            (failure) => emit(AuthState.authenticated(authEntity, message: failure.message)),
+            (newAccessToken) {
+              log('prev auth token: ${authEntity.accessToken}');
+              final newAuth = authEntity.copyWith(accessToken: newAccessToken);
+              log('new auth token: ${newAuth.accessToken}');
+              // save to local storage
+              _authRepository.cacheAuth(newAuth);
+              emit(AuthState.authenticated(newAuth));
+            },
           );
         },
       );
@@ -64,8 +69,7 @@ class AuthCubit extends Cubit<AuthState> {
       ),
     ).then((respEither) {
       respEither.fold(
-        (failure) =>
-            emit(AuthState.error(code: failure.code, message: failure.message)),
+        (failure) => emit(AuthState.error(code: failure.code, message: failure.message)),
         (ok) => emit(AuthState.authenticated(ok.data,
             message: kMsgLoggedInSuccessfully, code: 200, redirectTo: '/home')),
       );
@@ -76,8 +80,7 @@ class AuthCubit extends Cubit<AuthState> {
     emit(const AuthState.authenticating());
     await _logoutUC(refreshToken).then((respEither) {
       respEither.fold(
-        (error) =>
-            emit(AuthState.error(code: error.code, message: error.message)),
+        (error) => emit(AuthState.error(code: error.code, message: error.message)),
         (ok) => emit(AuthState.unauthenticated(
           message: ok.message,
           code: ok.code,
@@ -90,8 +93,7 @@ class AuthCubit extends Cubit<AuthState> {
     emit(const AuthState.authenticating());
     await _authRepository.register(params).then((resultEither) {
       resultEither.fold(
-        (error) =>
-            emit(AuthState.error(code: error.code, message: error.message)),
+        (error) => emit(AuthState.error(code: error.code, message: error.message)),
         (ok) => emit(AuthState.unauthenticated(
           message: ok.message,
           code: ok.code,
@@ -101,20 +103,16 @@ class AuthCubit extends Cubit<AuthState> {
     });
   }
 
-  Future<void> changePassword(
-      {required String oldPassword, required String newPassword}) async {
+  Future<void> changePassword({required String oldPassword, required String newPassword}) async {
     // using 'state' to get the previous state (should be authenticated)
     final previousState = state;
     emit(const AuthState.authenticating());
-    await _authRepository
-        .changePassword(oldPassword, newPassword)
-        .then((resultEither) {
+    await _authRepository.changePassword(oldPassword, newPassword).then((resultEither) {
       //? even user change password success or not, keep the user authenticated
       resultEither.fold(
-        (error) => emit(
-            previousState.copyWith(message: error.message, code: error.code)),
-        (ok) => emit(previousState.copyWith(
-            message: ok.message, code: ok.code, redirectTo: '/user')),
+        (error) => emit(previousState.copyWith(message: error.message, code: error.code)),
+        (ok) =>
+            emit(previousState.copyWith(message: ok.message, code: ok.code, redirectTo: '/user')),
       );
     });
   }
@@ -126,11 +124,9 @@ class AuthCubit extends Cubit<AuthState> {
     await _authRepository.editUserProfile(newInfo).then((resultEither) {
       //? even user change password success or not, keep the user authenticated
       resultEither.fold(
-        (error) => emit(
-            previousState.copyWith(message: error.message, code: error.code)),
+        (error) => emit(previousState.copyWith(message: error.message, code: error.code)),
         (ok) => emit(AuthState.authenticated(
-          previousState.auth!
-              .copyWith(userInfo: ok.data), // copy with new user info
+          previousState.auth!.copyWith(userInfo: ok.data), // copy with new user info
           message: ok.message,
           code: ok.code,
           redirectTo: '/user',
